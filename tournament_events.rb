688d82e1148e28bd51ef2759502939ff8608e138
Aleksey Kurepin <lesha@kurepin.com>
2014-02-06 17:37:38 +0400

    init

---
+++ tournament_events.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+require 'nokogiri'
+require 'open-uri'
+
+CSV_PATH = 'sochi_tournament_events.csv'
+events_array = []
+
+doc = Nokogiri::XML(open('http://api.sport.yandex.ru/public/tournament_events.xml'))
+doc.css('event').each do |event|
+  events_array << {
+    title: event['event_title'],
+    competition_id: event['competition_id'],
+    event_id: event['id'],
+    time: DateTime.parse(event.css('time').inner_text).strftime('%F %R')
+  }
+end
+
+events_array.sort!{ |a,b| a[:time] <=> b[:time] }
+
+File.open(CSV_PATH, 'w') do |file|
+  line = "Date/time;Title;competition_id;event_id\n"
+  file.write(line)
+  events_array.each do |event|
+    line = "#{event[:time]};#{event[:title]};#{event[:competition_id]};#{event[:event_id]}\n"
+    file.write(line)
+  end
+end
